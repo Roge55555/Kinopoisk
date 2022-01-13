cdf6502d6aa5767b1a601838d11236f128f981dc
Roge55555 <roge55555@gmail.com>
2022-01-13 13:20:13 +0300

    add controller

--- src/main/java/com/itech/kinopoisk/controller/FilmController.java
+++ src/main/java/com/itech/kinopoisk/controller/FilmController.java
@@ -1,11 +1,72 @@
 package com.itech.kinopoisk.controller;
 
+import com.itech.kinopoisk.entity.Film;
+import com.itech.kinopoisk.model.dto.FilmAddDTO;
+import com.itech.kinopoisk.model.dto.FilmUpdateDTO;
+import com.itech.kinopoisk.service.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/films")
 public class FilmController {
+
+    private final FilmService filmService;
+
+    private final CountryOfFilmService countryOfFilmService;
+
+    private final GenreOfFilmService genreOfFilmService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film addFilm(@Valid @RequestBody FilmAddDTO filmAddDTO) {
+        Film film = Film.builder()
+                .name_ru(filmAddDTO.getNameRu())
+                .name_en(filmAddDTO.getNameEn())
+                .year(filmAddDTO.getYear())
+                .length(filmAddDTO.getFilmLength())
+                .rating(filmAddDTO.getRating())
+                .ratingVoteCount(filmAddDTO.getRatingVoteCount())
+                .poster_url(filmAddDTO.getPosterUrl())
+                .poster_url_preview(filmAddDTO.getPosterUrlPreview())
+                .build();
+
+        film = filmService.add(film);
+        countryOfFilmService.add(film.getId(), filmAddDTO.getCountries());
+        genreOfFilmService.add(film.getId(), filmAddDTO.getGenres());
+
+        return filmService.add(film);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateUser(@Valid @RequestBody FilmUpdateDTO filmUpdateDTO) {
+        Film film = Film.builder()
+                .id(filmUpdateDTO.getFilmId())
+                .name_ru(filmUpdateDTO.getNameRu())
+                .name_en(filmUpdateDTO.getNameEn())
+                .year(filmUpdateDTO.getYear())
+                .length(filmUpdateDTO.getFilmLength())
+                .rating(filmUpdateDTO.getRating())
+                .ratingVoteCount(filmUpdateDTO.getRatingVoteCount())
+                .poster_url(filmUpdateDTO.getPosterUrl())
+                .poster_url_preview(filmUpdateDTO.getPosterUrlPreview())
+                .build();
+
+        film = filmService.update(film);
+        countryOfFilmService.update(film.getId(), filmUpdateDTO.getCountries());
+        genreOfFilmService.update(film.getId(), filmUpdateDTO.getGenres());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFilm(@PathVariable("id") Long id) {
+        filmService.delete(id);
+        countryOfFilmService.deleteByFilmId(id);
+        genreOfFilmService.deleteByFilmId(id);
+    }
 }
